78a548e90db1b145bdbe79ac6a42f58e7d1818e2
quibbler01 <fluent0418@gmail.com>
2022-08-14 14:35:02 +0800

    add implementation of DiskCache:BaseCache、LimitedAgaDiskCache、UnlimitedDiskCache

---
+++ ImageLoader/src/main/java/cn/quibbler/imageloader/cache/disk/impl/BaseDiskCache.kt
@@ -0,0 +1,43 @@
+package cn.quibbler.imageloader.cache.disk.impl
+
+import android.graphics.Bitmap
+import cn.quibbler.imageloader.cache.disk.DiskCache
+import cn.quibbler.imageloader.utils.CopyListener
+import java.io.File
+import java.io.InputStream
+
+class BaseDiskCache : DiskCache {
+
+    override fun getDirectory(): File? {
+        TODO("Not yet implemented")
+    }
+
+    override fun get(imageUrl: String?): File? {
+        TODO("Not yet implemented")
+    }
+
+    override fun save(
+        imageUrl: String?,
+        imageStream: InputStream?,
+        listener: CopyListener?
+    ): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun save(imageUrl: String?, bitmap: Bitmap?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun remove(imageUrl: String?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun close() {
+        TODO("Not yet implemented")
+    }
+
+    override fun clear() {
+        TODO("Not yet implemented")
+    }
+
+}
