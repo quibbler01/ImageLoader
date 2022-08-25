c1bc6f7bff543e0d0f640128bcb6fac05d8aa5b7
quibbler01 <fluent0418@gmail.com>
2022-08-26 00:32:23 +0800

    finish ImageLoader/src/main/java/cn/quibbler/imageloader/cache/disk/impl/UnlimitedDiskCache.kt

--- ImageLoader/src/main/java/cn/quibbler/imageloader/cache/disk/impl/UnlimitedDiskCache.kt
+++ ImageLoader/src/main/java/cn/quibbler/imageloader/cache/disk/impl/UnlimitedDiskCache.kt
@@ -1,43 +1,21 @@
 package cn.quibbler.imageloader.cache.disk.impl
 
 import android.graphics.Bitmap
-import cn.quibbler.imageloader.cache.disk.DiskCache
+import cn.quibbler.imageloader.cache.disk.naming.FileNameGenerator
 import cn.quibbler.imageloader.utils.CopyListener
 import java.io.File
 import java.io.InputStream
 
-class UnlimitedDiskCache : DiskCache {
+class UnlimitedDiskCache : BaseDiskCache {
 
-    override fun getDirectory(): File? {
-        TODO("Not yet implemented")
-    }
+    constructor(cacheDir: File) : super(cacheDir)
 
-    override fun get(imageUrl: String?): File? {
-        TODO("Not yet implemented")
-    }
+    constructor(cacheDir: File?, reserveCacheDir: File?) : super(cacheDir, reserveCacheDir)
 
-    override fun save(
-        imageUrl: String?,
-        imageStream: InputStream?,
-        listener: CopyListener?
-    ): Boolean {
-        TODO("Not yet implemented")
-    }
-
-    override fun save(imageUrl: String?, bitmap: Bitmap?): Boolean {
-        TODO("Not yet implemented")
-    }
-
-    override fun remove(imageUrl: String?) {
-        TODO("Not yet implemented")
-    }
-
-    override fun close() {
-        TODO("Not yet implemented")
-    }
-
-    override fun clear() {
-        TODO("Not yet implemented")
-    }
+    constructor(
+        cacheDir: File?,
+        reserveCacheDir: File?,
+        fileNameGenerator: FileNameGenerator?
+    ) : super(cacheDir, reserveCacheDir, fileNameGenerator)
 
 }
