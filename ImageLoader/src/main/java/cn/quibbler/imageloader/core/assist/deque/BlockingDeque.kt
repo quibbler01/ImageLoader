d30d8740c563f626130774bdc29d3a1492e5938b
quibbler01 <fluent0418@gmail.com>
2022-09-01 00:07:17 +0800

    complete Deque interface

--- ImageLoader/src/main/java/cn/quibbler/imageloader/core/assist/deque/BlockingDeque.kt
+++ ImageLoader/src/main/java/cn/quibbler/imageloader/core/assist/deque/BlockingDeque.kt
@@ -2,7 +2,74 @@ package cn.quibbler.imageloader.core.assist.deque
 
 import java.util.Deque
 import java.util.concurrent.BlockingDeque
+import java.util.concurrent.TimeUnit
 
 interface BlockingDeque<E> : BlockingDeque<E>, Deque<E> {
+
+    override fun addFirst(e: E)
+
+    override fun addLast(e: E)
+
+    override fun offerFirst(e: E): Boolean
+
+    override fun offerLast(e: E): Boolean
+
+    @Throws(InterruptedException::class)
+    override fun putFirst(e: E)
+
+    @Throws(InterruptedException::class)
+    override fun putLast(e: E)
+
+    @Throws(InterruptedException::class)
+    override fun offerFirst(e: E, timeout: Long, unit: TimeUnit): Boolean
+
+    @Throws(InterruptedException::class)
+    override fun offerLast(e: E, timeout: Long, unit: TimeUnit): Boolean
+
+    @Throws(InterruptedException::class)
+    override fun takeFirst(): E
+
+    @Throws(InterruptedException::class)
+    override fun pollFirst(timeout: Long, unit: TimeUnit): E
+
+    @Throws(InterruptedException::class)
+    override fun pollLast(timeout: Long, unit: TimeUnit): E
+
+    override fun removeFirstOccurrence(o: Any?): Boolean
+
+    override fun removeLastOccurrence(a: Any?): Boolean
+
+    override fun add(e: E): Boolean
+
+    override fun offer(e: E): Boolean
+
+    @Throws(InterruptedException::class)
+    override fun put(e: E)
+
+    override fun offer(e: E, timeout: Long, unit: TimeUnit): Boolean
+
+    override fun remove(): E
+
+    override fun poll(): E
+
+    @Throws(InterruptedException::class)
+    override fun take(): E
+
+    @Throws(InterruptedException::class)
+    override fun poll(timeout: Long, unit: TimeUnit): E
+
+    override fun element(): E
+
+    override fun peek(): E
+
+    override fun remove(element: E?): Boolean
+
+    override fun contains(element: E?): Boolean
+
+    override val size: Int
+
+    override fun iterator(): MutableIterator<E>
+
+    override fun push(e: E)
 
 }
--- ImageLoader/src/main/java/cn/quibbler/imageloader/core/assist/deque/Deque.kt
+++ ImageLoader/src/main/java/cn/quibbler/imageloader/core/assist/deque/Deque.kt
@@ -1,5 +1,61 @@
 package cn.quibbler.imageloader.core.assist.deque
 
-interface Deque {
+import java.util.*
+
+interface Deque<E> : Queue<E> {
+
+    fun addFirst(e: E)
+
+    fun addLast(e: E)
+
+    fun offerFirst(e: E): Boolean
+
+    fun offerLast(e: E): Boolean
+
+    fun removeFirst(): E
+
+    fun removeLast(): E
+
+    fun pollFirst(): E
+
+    fun pollLast(): E
+
+    fun getFirst(): E
+
+    fun getLast(): E
+
+    fun peekFirst(): E
+
+    fun peekLast(): E
+
+    fun removeFirstOccurrence(a: Any): Boolean
+
+    fun removeLastOccurrence(a: Any): Boolean
+
+    override fun add(e: E): Boolean
+
+    override fun offer(e: E): Boolean
+
+    override fun remove(): E
+
+    override fun poll(): E
+
+    override fun element(): E
+
+    override fun peek(): E
+
+    fun push(e: E)
+
+    fun pop(): E
+
+    override fun remove(element: E): Boolean
+
+    override fun contains(element: E): Boolean
+
+    override val size: Int
+
+    override fun iterator(): MutableIterator<E>
+
+    fun descendingIterator(): Iterator<E>
 
 }
