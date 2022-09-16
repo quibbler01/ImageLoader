f45c551e335914b033ed6551c6b565d0f61c8b1d
quibbler01 <fluent0418@gmail.com>
2022-09-16 21:03:12 +0800

    add BitmapProcessor interface

---
+++ ImageLoader/src/main/java/cn/quibbler/imageloader/core/process/BitmapProcessor.kt
@@ -0,0 +1,23 @@
+package cn.quibbler.imageloader.core.process
+
+import android.graphics.Bitmap
+
+/**
+ * Makes some processing on [Bitmap]. Implementations can apply any changes to original {@link Bitmap}.<br />
+ * Implementations have to be thread-safe.
+ */
+interface BitmapProcessor {
+
+    /**
+     * Makes some processing of incoming bitmap.<br />
+     * This method is executing on additional thread (not on UI thread).<br />
+     * <b>Note:</b> If this processor is used as {@linkplain DisplayImageOptions.Builder#preProcessor(BitmapProcessor)
+     * pre-processor} then don't forget {@linkplain Bitmap#recycle() to recycle} incoming bitmap if you return a new
+     * created one.
+     *
+     * @param bitmap Original [Bitmap]
+     * @return Processed [Bitmap]
+     */
+    fun process(bitmap: Bitmap): Bitmap
+
+}
