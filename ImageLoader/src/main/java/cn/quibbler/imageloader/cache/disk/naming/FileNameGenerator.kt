f2952f7f546166ce2f5b0dfe28a0d8bb857a5737
quibbler01 <fluent0418@gmail.com>
2022-10-09 21:36:32 +0800

    change FileNameGenerator

--- ImageLoader/src/main/java/cn/quibbler/imageloader/cache/disk/naming/FileNameGenerator.kt
+++ ImageLoader/src/main/java/cn/quibbler/imageloader/cache/disk/naming/FileNameGenerator.kt
@@ -1,7 +1,14 @@
 package cn.quibbler.imageloader.cache.disk.naming
 
+/**
+ * Generates names for files at disk cache
+ *
+ * @author Sergey Tarasevich (nostra13[at]gmail[dot]com)
+ * @since 1.3.1
+ */
 interface FileNameGenerator {
 
+    /** Generates unique file name for image defined by URI */
     fun generate(imageUrl: String): String
 
 }
