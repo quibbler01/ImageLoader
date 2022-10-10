e44a933e5d90b530335f9e1980bab495411338be
quibbler01 <fluent0418@gmail.com>
2022-10-10 12:23:51 +0800

    change HashCodeFileNameGenerator class

--- ImageLoader/src/main/java/cn/quibbler/imageloader/cache/disk/naming/HashCodeFileNameGenerator.kt
+++ ImageLoader/src/main/java/cn/quibbler/imageloader/cache/disk/naming/HashCodeFileNameGenerator.kt
@@ -1,5 +1,11 @@
 package cn.quibbler.imageloader.cache.disk.naming
 
+/**
+ * Names image file as image URI {@linkplain String#hashCode() hashcode}
+ *
+ * @author Sergey Tarasevich (nostra13[at]gmail[dot]com)
+ * @since 1.3.1
+ */
 class HashCodeFileNameGenerator : FileNameGenerator {
 
     override fun generate(imageUrl: String): String = imageUrl.hashCode().toString()
