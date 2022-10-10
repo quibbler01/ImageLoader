cf75cf14a2bce3d86d0161ed337616de784de6e3
quibbler01 <fluent0418@gmail.com>
2022-10-10 12:23:44 +0800

    change FuzzyKeyMemoryCache class

--- ImageLoader/src/main/java/cn/quibbler/imageloader/cache/memory/impl/FuzzyKeyMemoryCache.kt
+++ ImageLoader/src/main/java/cn/quibbler/imageloader/cache/memory/impl/FuzzyKeyMemoryCache.kt
@@ -3,12 +3,22 @@ package cn.quibbler.imageloader.cache.memory.impl
 import android.graphics.Bitmap
 import cn.quibbler.imageloader.cache.memory.MemoryCache
 
+/**
+ * Decorator for {@link MemoryCache}. Provides special feature for cache: some different keys are considered as
+ * equals (using {@link Comparator comparator}). And when you try to put some value into cache by key so entries with
+ * "equals" keys will be removed from cache before.<br />
+ * <b>NOTE:</b> Used for internal needs. Normally you don't need to use this class.
+ *
+ * @author Sergey Tarasevich (nostra13[at]gmail[dot]com)
+ * @since 1.0.0
+ */
 class FuzzyKeyMemoryCache(
     private val cache: MemoryCache,
     private val keyComparator: Comparator<String>
 ) : MemoryCache {
 
     override fun put(key: String, value: Bitmap): Boolean {
+        // Search equal key and remove this entry
         synchronized(cache) {
             var keyToRemove: String? = null
             for (cacheKey in cache.keys()) {
