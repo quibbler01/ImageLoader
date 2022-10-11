f7b3b73c283473829ee13a54fca2021968071705
quibbler01 <fluent0418@gmail.com>
2022-10-11 21:44:41 +0800

    change ImageDecoder class

--- ImageLoader/src/main/java/cn/quibbler/imageloader/core/decode/ImageDecoder.kt
+++ ImageLoader/src/main/java/cn/quibbler/imageloader/core/decode/ImageDecoder.kt
@@ -3,9 +3,23 @@ package cn.quibbler.imageloader.core.decode
 import android.graphics.Bitmap
 import java.io.IOException
 
+/**
+ * Provide decoding image to result {@link Bitmap}.
+ *
+ * @author Sergey Tarasevich (nostra13[at]gmail[dot]com)
+ * @see ImageDecodingInfo
+ * @since 1.8.3
+ */
 interface ImageDecoder {
 
+    /**
+     * Decodes image to {@link Bitmap} according target size and other parameters.
+     *
+     * @param imageDecodingInfo
+     * @return
+     * @throws IOException
+     */
     @Throws(IOException::class)
-    fun decode(imageDecodingInfo: ImageDecodingInfo): Bitmap
+    fun decode(imageDecodingInfo: ImageDecodingInfo): Bitmap?
 
 }
