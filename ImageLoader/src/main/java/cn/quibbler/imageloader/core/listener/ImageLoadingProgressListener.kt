1465cc2c4fdc73a82177979609bac18c9938a99a
quibbler01 <fluent0418@gmail.com>
2022-10-13 22:21:31 +0800

    change ImageLoadingProgressListener class

--- ImageLoader/src/main/java/cn/quibbler/imageloader/core/listener/ImageLoadingProgressListener.kt
+++ ImageLoader/src/main/java/cn/quibbler/imageloader/core/listener/ImageLoadingProgressListener.kt
@@ -2,8 +2,22 @@ package cn.quibbler.imageloader.core.listener
 
 import android.view.View
 
+/**
+ * Listener for image loading progress.
+ *
+ * @author Sergey Tarasevich (nostra13[at]gmail[dot]com)
+ * @since 1.9.1
+ */
 interface ImageLoadingProgressListener {
 
+    /**
+     * Is called when image loading progress changed.
+     *
+     * @param imageUri Image URI
+     * @param view     View for image. Can be <b>null</b>.
+     * @param current  Downloaded size in bytes
+     * @param total    Total size in bytes
+     */
     fun onProgressUpdate(imageUri: String, view: View, current: Int, total: Int)
 
 }
