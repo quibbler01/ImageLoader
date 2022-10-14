7c12201446294ea41611e4faeed96f2e24b136fd
quibbler01 <fluent0418@gmail.com>
2022-10-14 22:08:30 +0800

    change ImageSizeUtils class

--- ImageLoader/src/main/java/cn/quibbler/imageloader/utils/ImageSizeUtils.kt
+++ ImageLoader/src/main/java/cn/quibbler/imageloader/utils/ImageSizeUtils.kt
@@ -19,6 +19,11 @@ private val maxBitmapSize: ImageSize
         return ImageSize(maxBitmapDimension, maxBitmapDimension)
     }
 
+/**
+ * Defines target size for image aware view. Size is defined by target
+ * {@link com.nostra13.universalimageloader.core.imageaware.ImageAware view} parameters, configuration
+ * parameters or device display dimensions.<br />
+ */
 fun defineTargetSizeForView(imageAware: ImageAware, maxImageSize: ImageSize): ImageSize {
     var width = imageAware.getWidth()
     if (width <= 0) {
@@ -37,6 +42,33 @@ fun defineTargetSizeForView(imageAware: ImageAware, maxImageSize: ImageSize): Im
     return ImageSize(width, height)
 }
 
+/**
+ * Computes sample size for downscaling image size (<b>srcSize</b>) to view size (<b>targetSize</b>). This sample
+ * size is used during
+ * {@linkplain BitmapFactory#decodeStream(java.io.InputStream, android.graphics.Rect, android.graphics.BitmapFactory.Options)
+ * decoding image} to bitmap.<br />
+ * <br />
+ * <b>Examples:</b><br />
+ * <p/>
+ * <pre>
+ * srcSize(100x100), targetSize(10x10), powerOf2Scale = true -> sampleSize = 8
+ * srcSize(100x100), targetSize(10x10), powerOf2Scale = false -> sampleSize = 10
+ *
+ * srcSize(100x100), targetSize(20x40), viewScaleType = FIT_INSIDE -> sampleSize = 5
+ * srcSize(100x100), targetSize(20x40), viewScaleType = CROP       -> sampleSize = 2
+ * </pre>
+ * <p/>
+ * <br />
+ * The sample size is the number of pixels in either dimension that correspond to a single pixel in the decoded
+ * bitmap. For example, inSampleSize == 4 returns an image that is 1/4 the width/height of the original, and 1/16
+ * the number of pixels. Any value <= 1 is treated the same as 1.
+ *
+ * @param srcSize       Original (image) size
+ * @param targetSize    Target (view) size
+ * @param viewScaleType {@linkplain ViewScaleType Scale type} for placing image in view
+ * @param powerOf2Scale <i>true</i> - if sample size be a power of 2 (1, 2, 4, 8, ...)
+ * @return Computed sample size
+ */
 fun computeImageSampleSize(srcSize: ImageSize, targetSize: ImageSize, viewScaleType: ViewScaleType, powerOf2Scale: Boolean): Int {
     val srcWidth = srcSize.width
     val srcHeight = srcSize.height
@@ -50,11 +82,11 @@ fun computeImageSampleSize(srcSize: ImageSize, targetSize: ImageSize, viewScaleT
             if (powerOf2Scale) {
                 val halfWidth = srcWidth / 2
                 val halfHeight = srcHeight / 2
-                while ((halfWidth / scale) > targetWidth || (halfHeight / scale) > targetHeight) {
+                while ((halfWidth / scale) > targetWidth || (halfHeight / scale) > targetHeight) {// ||
                     scale *= 2
                 }
             } else {
-                scale = max(srcWidth / targetWidth, srcHeight / targetHeight)
+                scale = max(srcWidth / targetWidth, srcHeight / targetHeight) // max
             }
         }
         ViewScaleType.CROP -> {
@@ -92,6 +124,15 @@ fun considerMaxTextureSize(srcWidth: Int, srcHeight: Int, scale: Int, powerOf2: 
     return scale_
 }
 
+/**
+ * Computes minimal sample size for downscaling image so result image size won't exceed max acceptable OpenGL
+ * texture size.<br />
+ * We can't create Bitmap in memory with size exceed max texture size (usually this is 2048x2048) so this method
+ * calculate minimal sample size which should be applied to image to fit into these limits.
+ *
+ * @param srcSize Original image size
+ * @return Minimal sample size
+ */
 fun computeMinImageSampleSize(srcSize: ImageSize): Int {
     val srcWidth = srcSize.width
     val srcHeight = srcSize.height
@@ -102,9 +143,31 @@ fun computeMinImageSampleSize(srcSize: ImageSize): Int {
     val widthScale = ceil(srcWidth.toFloat() / targetWidth).toInt()
     val heightScale = ceil(srcHeight.toFloat() / targetHeight).toInt()
 
-    return max(widthScale, heightScale)
+    return max(widthScale, heightScale) // max
 }
 
+/**
+ * Computes scale of target size (<b>targetSize</b>) to source size (<b>srcSize</b>).<br />
+ * <br />
+ * <b>Examples:</b><br />
+ * <p/>
+ * <pre>
+ * srcSize(40x40), targetSize(10x10) -> scale = 0.25
+ *
+ * srcSize(10x10), targetSize(20x20), stretch = false -> scale = 1
+ * srcSize(10x10), targetSize(20x20), stretch = true  -> scale = 2
+ *
+ * srcSize(100x100), targetSize(20x40), viewScaleType = FIT_INSIDE -> scale = 0.2
+ * srcSize(100x100), targetSize(20x40), viewScaleType = CROP       -> scale = 0.4
+ * </pre>
+ *
+ * @param srcSize       Source (image) size
+ * @param targetSize    Target (view) size
+ * @param viewScaleType {@linkplain ViewScaleType Scale type} for placing image in view
+ * @param stretch       Whether source size should be stretched if target size is larger than source size. If <b>false</b>
+ *                      then result scale value can't be greater than 1.
+ * @return Computed scale
+ */
 fun computeImageScale(srcSize: ImageSize, targetSize: ImageSize, viewScaleType: ViewScaleType, stretch: Boolean): Float {
     val srcWidth = srcSize.width
     val srcHeight = srcSize.height
