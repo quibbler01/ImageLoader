29ec05dbd128db2609e00895295b08af06e4d022
quibbler01 <fluent0418@gmail.com>
2022-10-16 00:42:10 +0800

    change LimitedAgeMemoryCache class

--- ImageLoader/src/main/java/cn/quibbler/imageloader/cache/memory/impl/LimitedAgeMemoryCache.kt
+++ ImageLoader/src/main/java/cn/quibbler/imageloader/cache/memory/impl/LimitedAgeMemoryCache.kt
@@ -4,10 +4,33 @@ import android.graphics.Bitmap
 import cn.quibbler.imageloader.cache.memory.MemoryCache
 import java.util.*
 import kotlin.collections.HashMap
+import kotlin.math.max
 
-class LimitedAgeMemoryCache(private val cache: MemoryCache, private val maxAge: Long) : MemoryCache {
+/**
+ * Decorator for {@link MemoryCache}. Provides special feature for cache: if some cached object age exceeds defined
+ * value then this object will be removed from cache.
+ *
+ * @author Sergey Tarasevich (nostra13[at]gmail[dot]com)
+ * @see MemoryCache
+ * @since 1.3.1
+ */
+class LimitedAgeMemoryCache : MemoryCache {
+
+    private val cache: MemoryCache
+
+    private val maxAge: Long
 
     private val loadingDates = Collections.synchronizedMap(HashMap<String, Long>())
+
+    /**
+     * @param cache  Wrapped memory cache
+     * @param maxAge Max object age <b>(in seconds)</b>. If object age will exceed this value then it'll be removed from
+     *               cache on next treatment (and therefore be reloaded).
+     */
+    constructor(cache: MemoryCache, maxAge: Long) {
+        this.cache = cache
+        this.maxAge = maxAge * 1000 // to milliseconds
+    }
 
     override fun put(key: String, value: Bitmap): Boolean {
         val putSuccesfully = cache.put(key, value)
