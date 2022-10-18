f19b82467f84914973f431675749d12ffe0076b2
quibbler01 <fluent0418@gmail.com>
2022-10-19 00:02:22 +0800

    change MemoryCache class

--- ImageLoader/src/main/java/cn/quibbler/imageloader/cache/memory/MemoryCache.kt
+++ ImageLoader/src/main/java/cn/quibbler/imageloader/cache/memory/MemoryCache.kt
@@ -2,16 +2,32 @@ package cn.quibbler.imageloader.cache.memory
 
 import android.graphics.Bitmap
 
+/**
+ * Interface for memory cache
+ *
+ * @author Sergey Tarasevich (nostra13[at]gmail[dot]com)
+ * @since 1.9.2
+ */
 interface MemoryCache {
 
+    /**
+     * Puts value into cache by key
+     *
+     * @return <b>true</b> - if value was put into cache successfully, <b>false</b> - if value was <b>not</b> put into
+     * cache
+     */
     fun put(key: String, value: Bitmap): Boolean
 
+    /** Returns value by key. If there is no value for key then null will be returned. */
     fun get(key: String): Bitmap?
 
+    /** Removes item by key */
     fun remove(key: String): Bitmap?
 
+    /** Returns all keys of cache */
     fun keys(): Collection<String>
 
+    /** Remove all items from cache */
     fun clear()
 
 }
