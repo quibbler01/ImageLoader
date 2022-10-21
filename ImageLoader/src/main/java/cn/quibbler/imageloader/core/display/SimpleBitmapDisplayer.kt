5a56b9f538dedb06f532a99784a4a875724c4b82
quibbler01 <fluent0418@gmail.com>
2022-10-21 12:38:29 +0800

    change SimpleBitmapDisplayer class

--- ImageLoader/src/main/java/cn/quibbler/imageloader/core/display/SimpleBitmapDisplayer.kt
+++ ImageLoader/src/main/java/cn/quibbler/imageloader/core/display/SimpleBitmapDisplayer.kt
@@ -4,6 +4,12 @@ import android.graphics.Bitmap
 import cn.quibbler.imageloader.core.assist.LoadedFrom
 import cn.quibbler.imageloader.core.imageaware.ImageAware
 
+/**
+ * Just displays {@link Bitmap} in {@link com.nostra13.universalimageloader.core.imageaware.ImageAware}
+ *
+ * @author Sergey Tarasevich (nostra13[at]gmail[dot]com)
+ * @since 1.5.6
+ */
 class SimpleBitmapDisplayer : BitmapDisplayer {
 
     override fun display(bitmap: Bitmap, imageAware: ImageAware, loadedFrom: LoadedFrom) {
