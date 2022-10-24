d2fae5a52fe791f0338bd303f3f7cb128fa7e504
quibbler01 <fluent0418@gmail.com>
2022-10-24 21:54:26 +0800

    change WeakMemoryCache class

--- ImageLoader/src/main/java/cn/quibbler/imageloader/cache/memory/impl/WeakMemoryCache.kt
+++ ImageLoader/src/main/java/cn/quibbler/imageloader/cache/memory/impl/WeakMemoryCache.kt
@@ -5,6 +5,14 @@ import cn.quibbler.imageloader.cache.memory.BaseMemoryCache
 import java.lang.ref.Reference
 import java.lang.ref.WeakReference
 
+/**
+ * Memory cache with {@linkplain WeakReference weak references} to {@linkplain android.graphics.Bitmap bitmaps}<br />
+ * <br />
+ * <b>NOTE:</b> This cache uses only weak references for stored Bitmaps.
+ *
+ * @author Sergey Tarasevich (nostra13[at]gmail[dot]com)
+ * @since 1.5.3
+ */
 class WeakMemoryCache : BaseMemoryCache() {
 
     override fun createReference(value: Bitmap): Reference<Bitmap> {
